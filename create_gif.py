2db3c6e6434ded810ed8867077d6c0001c88967a
Brandon Klein <klein.brandon@web.de>
2025-03-20 13:09:52 +0100

    Delete create_gif.py

--- create_gif.py
+++
@@ -1,9 +0,0 @@
-import imageio.v3 as iio
-
-filenames = ['nyan-cat1.png', 'nyan-cat2.png', 'nyan-cat3.png']
-images = []
-
-for filename in filenames:
-  images.append(iio.imread(filename))
-
-iio.imwrite('nyan-cat.gif', images, duration = 1000 / 3, loop = 0)
